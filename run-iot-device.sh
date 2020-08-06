1e0cff061fb4751a6f609ef2c33d27655612aae9
Vitalii Kulanov <vkulanov@waverleysoftware.com>
2020-08-06 12:40:10 +0300

    IMA2-69: Add ability to run IoT device simulator in background

--- run-iot-device.sh
+++ run-iot-device.sh
@@ -1,17 +1,35 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
+
+print_usage() {
+  echo "No DEVICE_ID argument specified and environment variable GCP_DEVICE_ID is not set" >&2
+  echo "" >&2
+  echo "Usage: run-iot-device.sh [-d GCP_DEVICE_ID] [-b]" >&2
+  echo "    -d GCP_DEVICE_ID    - run GCP_DEVICE_ID device" >&2
+  echo "    -b                  - run in background" >&2
+  exit 1
+}
 
 # Initialize environment variables that are defined in .env file
 echo "[*] Initializing environment variables from '.env'..."
 export $(grep -v '^#' .env | xargs)
 
-# Option to override GCP_DEVICE_ID from the first argument
-GCP_DEVICE_ID="${1:-$GCP_DEVICE_ID}"
+b_flag=''
+GCP_DEVICE_ID=$GCP_DEVICE_ID
 
-if [ -z "${GCP_DEVICE_ID}" ]; then
-  echo "No DEVICE_ID argument and environment variable GCP_DEVICE_ID is not set" >&2
-  echo "Usage: run-iot-device.sh [GCP_DEVICE_ID]" >&2
+while getopts 'bd:' flag; do
+  case "${flag}" in
+    b)  b_flag='true' ;;
+    d)  GCP_DEVICE_ID="${OPTARG}" ;;
+    *)  print_usage
+        exit 1 ;;
+  esac
+done
+
+# No $GCP_DEVICE_ID specified + forbid positional arguments (only flags)
+if [ -z "$GCP_DEVICE_ID" ] || [ "${@:$OPTIND:1}" ]; then
+  print_usage
   exit 1
 fi
 
@@ -21,9 +39,22 @@ gcloud beta iot devices describe "$GCP_DEVICE_ID" \
   --region="$GCP_REGION" \
   --registry="$GCP_IOT_REGISTRY_NAME"
 
-# Run simulator
+# Run IoT device simulator
 echo "[*] Running '$GCP_DEVICE_ID'..."
-gcloud compute ssh "$GCP_VM_SIMULATOR_NAME" --zone="$GCP_ZONE" -- \
+if [ "$b_flag" = true ] ; then
+  gcloud compute ssh "$GCP_VM_SIMULATOR_NAME" --zone="$GCP_ZONE" \
+    --command="nohup \
+     python3.7 cloud_iot_mqtt.py \
+       --project_id=$GCP_PROJECT \
+       --registry_id=$GCP_IOT_REGISTRY_NAME \
+       --device_id=$GCP_DEVICE_ID \
+       --private_key_file='/home/user/.ssh/rsa_private.pem' \
+       --algorithm=RS256 \
+       --ca_certs='/home/user/.ssh/roots.pem' \
+       --num_messages 100 \
+     > /dev/null 2>&1 &"
+else
+  gcloud compute ssh "$GCP_VM_SIMULATOR_NAME" --zone="$GCP_ZONE" -- \
    "python3.7 cloud_iot_mqtt.py \
      --project_id=$GCP_PROJECT \
      --registry_id=$GCP_IOT_REGISTRY_NAME \
@@ -31,4 +62,5 @@ gcloud compute ssh "$GCP_VM_SIMULATOR_NAME" --zone="$GCP_ZONE" -- \
      --private_key_file='/home/user/.ssh/rsa_private.pem' \
      --algorithm=RS256 \
      --ca_certs='/home/user/.ssh/roots.pem' \
-     --num_messages 10"
+     --num_messages 1"
+fi
